34a436c4680ce6186918cb63a4bad87a6f4b1516
OfflineBrain <leonidivakhnenko@gmail.com>
2023-06-29 23:17:09 +0900

    vm repl

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,5 +4,5 @@ fun main(args: Array<String>) {
     println("Welcome to the Monkey programming language!")
     println("Feel free to type in commands")
 
-    Repl(System.`in`.bufferedReader(), PrintWriter(System.out)).start()
+    Repl(System.`in`.bufferedReader(), PrintWriter(System.out)).startVm()
 }
--- src/main/kotlin/Repl.kt
+++ src/main/kotlin/Repl.kt
@@ -1,4 +1,5 @@
 import ast.Parser
+import compiler.Compiler
 import eval.eval
 import token.Lexer
 import java.io.BufferedReader
@@ -20,6 +21,34 @@ class Repl(private val reader: BufferedReader, private val writer: PrintWriter) 
                 writer.println(parser.errors())
             } else {
                 writer.println(eval(program, env).render())
+            }
+
+            writer.flush()
+        }
+    }
+
+    fun startVm() {
+
+        while (true) {
+            writer.print(">> ")
+            writer.flush()
+            val line = reader.readLine() ?: break
+            val lexer = Lexer(line)
+            val parser = Parser(lexer)
+            val program = parser.parseProgram()
+
+            if (parser.errors.isNotEmpty()) {
+                writer.println(parser.errors())
+            } else {
+
+                val compiler = Compiler()
+                compiler.compile(program)
+                val vm = vm.Vm(compiler.bytecode())
+                vm.run()
+
+                vm.stackTop().let {
+                    writer.println(it.render())
+                }
             }
 
             writer.flush()
